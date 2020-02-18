2d93b18933fadc8034c2e5fbe4158cd0e1a230bf
Mia Mirecki <mia@dcc.team>
2020-02-18 10:29:50 +0100

    Added white background color to android pdf preview

--- lib/file_preview.dart
+++ lib/file_preview.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:path/path.dart' show extension;
 import 'package:pdf_previewer/pdf_previewer.dart';
@@ -20,22 +21,31 @@ class FilePreview {
     }
   }
 
-  static Future<Image> previewFactory(String filePath) async {
+  static Future<Widget> previewFactory(String filePath) async {
     switch (extension(filePath)) {
       case ".pdf":
         final filePreview =
             await PdfPreviewer.getPagePreview(filePath: filePath);
-        return Image.file(
-          File(filePreview),
-          fit: BoxFit.fitHeight,
-        );
+        return imageWithBackground(filePreview);
       case ".jpg":
       case ".jpeg":
       case ".png":
       case ".gif":
         return Image.file(File(filePath));
       default:
-        return Image.network("https://via.placeholder.com/80x100?text=${extension(filePath)}");
+        return Image.network(
+          "https://via.placeholder.com/80x100?text=${extension(filePath)}",
+        );
     }
+  }
+
+  static Widget imageWithBackground(String imagePath) {
+    return Container(
+      color: Colors.white,
+      child: Image.file(
+        File(imagePath),
+        fit: BoxFit.fitHeight,
+      ),
+    );
   }
 }
